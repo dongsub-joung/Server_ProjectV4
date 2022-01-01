c28a6fd8c489cdebaf093d78e2cae3bb80443567
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-01-01 09:23:32 +0900

    upload

--- UserMode_/src/main.rs
+++ UserMode_/src/main.rs
@@ -1,42 +1,36 @@
-use std::io;
+use std::{vec, fs::File};
 
 fn main() {
-    list_up_user_mode();
-
-    let mut input= String::new();
-    io::stdin().read_line(&mut input).expect("failed to read from stdin");
-    let number= input.trim();
-
-    match number.parse::<u8>() {
-        Ok(i) =>{
-            if i == 1 { download(); }
-            if i == 2 { upload(); }
-            if i == 3 { delete(); }
-            if (i>3) {
-                print!("return to menu");
-            }
-        },
-        Err(_) => {
-            print!("Not Integer!\r\n");
-        },
-    }
+    let mut files= vec!["asd","abc"];
+    let mut id= vec!["asdf","basd"];
+    let mut pw= vec!["123","3253as"];
     
-}
+    id.push("value");
+    pw.push("value2");
 
+// DW
+    // let ex= String::from("some");
+    // files.
 
-pub fn download(){
-    print!("DW");
-}
-pub fn upload(){
-    println!("UP");
-}
-pub fn delete(){
-    println!("DL");
-}
+// Upload
+    // files.push("value");
 
-pub fn list_up_user_mode(){
-    println!("1. Download File \r\n");
-	println!("2. Upload File   \r\n");
-	println!("3. Delete File   \r\n");
-	println!("Select a Service you use \r\n");
+    // remove
+    let removed= String::from("abc");
+    let mut index= 0;
+    for value in files.iter(){
+        if value.eq(&removed) {
+            break;
+        }
+        index += 1;
+    }
+    files.remove(index);
+
+    
+// TEst
+    print!("{}", files[0])
+
+    // for i in 0..{
+    //     print!("id: {}, pw: {}, file: {}\n", id[i], pw[i]);
+    // }
 }
---
+++ UserMode_/target/debug/deps/UserMode_-2830c63308b28abf.d
@@ -0,0 +1,5 @@
+/home/dongsub/git/Server_ProjectV4/UserMode_/target/debug/deps/UserMode_-2830c63308b28abf.rmeta: src/main.rs
+
+/home/dongsub/git/Server_ProjectV4/UserMode_/target/debug/deps/UserMode_-2830c63308b28abf.d: src/main.rs
+
+src/main.rs:
